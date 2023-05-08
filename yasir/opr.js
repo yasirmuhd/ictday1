b59c3ac51f284df1da6a5d29e1b49fff24ac5b8c
yasirmuhd <132889689+yasirmuhd@users.noreply.github.com>
2023-05-08 14:28:53 +0530

    dayi yasir ict6

---
+++ yasir/opr.js
@@ -0,0 +1,16 @@
+let per =
+{
+
+name : "yasir",
+
+age:"18",
+
+place : "pbvr",
+
+address:{
+    hname:"fajh" ,
+    post:"frsg"
+}
+};
+console.log(`mynameis ${per.name}`);
+//console.log(per.address);
